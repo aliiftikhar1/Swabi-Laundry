580a47a0a536f86578d6c4730f1df6c6c3766fbe
Ali Iftikhar <116260556+aliiftikhar1@users.noreply.github.com>
2024-06-29 11:12:52 +0500

    8th commit

--- src/components/whatsappbutton.js
+++ src/components/whatsappbutton.js
@@ -3,13 +3,10 @@ import { FaWhatsapp } from 'react-icons/fa';
 
 export default function WhatsAppButton() {
   return (
-    <a
-      href="https://wa.me/971589920080"
-      target="_blank"
-      rel="noopener noreferrer"
+    <Link href="https://wa.me/971589920080" target="_blank" rel="noopener noreferrer" 
       className="fixed bottom-4 right-4 w-16 h-16 bg-green-500 rounded-full shadow-lg flex items-center justify-center z-50 text-white text-3xl"
     >
       <FaWhatsapp />
-    </a>
+    </Link>
   );
 }
