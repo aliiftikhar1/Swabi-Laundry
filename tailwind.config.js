7a1d35a41429b8e2065cb017e73a3973ce77273d
Ali Iftikhar <116260556+aliiftikhar1@users.noreply.github.com>
2024-07-01 07:29:48 +0500

    17 commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,36 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//       animation: {
-//         'slide-left': 'slide-left 15s linear infinite',
-//       },
-//       keyframes: {
-//         'slide-left': {
-//           '0%': { transform: 'translateX(100%)' },
-//           '100%': { transform: 'translateX(-100%)' },
-//         },
-//       },
-//       filter: {
-//         'customBlue': 'invert(45%) sepia(100%) saturate(7000%) hue-rotate(182deg) brightness(94%) contrast(92%)',
-//       },
-//     },
-//   },
-//   variants: {
-//     extend: {
-//       filter: ['hover'],
-//     },
-//   },
-//   plugins: [
-//     require('tailwindcss-filters'),
-//   ],
-// };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        'slide-left': 'slide-left 15s linear infinite',
+      },
+      keyframes: {
+        'slide-left': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      filter: {
+        'customBlue': 'invert(45%) sepia(100%) saturate(7000%) hue-rotate(182deg) brightness(94%) contrast(92%)',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      filter: ['hover'],
+    },
+  },
+  plugins: [
+    require('tailwindcss-filters'),
+  ],
+};
