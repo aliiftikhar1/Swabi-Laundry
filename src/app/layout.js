0e67b6e521457d60ff4bdb07d8b3a5dfae1eec94
Ali Iftikhar <116260556+aliiftikhar1@users.noreply.github.com>
2024-07-01 07:35:12 +0500

    17 commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
 
 export const metadata = {
   title: "Swabi Laundry",
