03c43612d49064b277baf3acfb9dfeed5543aacb
Ali Iftikhar <116260556+aliiftikhar1@users.noreply.github.com>
2024-07-01 07:39:46 +0500

    17 commit

--- src/app/pages/pricing/page.js
+++ src/app/pages/pricing/page.js
@@ -1,16 +1,10 @@
 'use client'
-import Hero from "@/components/Herosection";
 import React from "react";
 import Navbar from "@/components/Header";
-import Tag from "./components/tag";
-import LaundryInfo from "./components/laundryinfo";
-import Productpropcomponent from "./components/productprop";
 import FAQSection from "./components/FAQsection";
 import PricingSection from "./components/pricingseciton";
-import Footer from "@/components/Footer";
-import Footer2 from "@/components/Footer2";
 import AboveFooter from "@/components/abovefooter";
-import ContactComponent from "@/components/contact";
+
 import Head from "next/head";
 export default function Home() {
   return (
