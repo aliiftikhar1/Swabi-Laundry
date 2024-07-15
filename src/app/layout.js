99cc14daeae94f5f43b0f1c496335ce427668c93
Ali Iftikhar <116260556+aliiftikhar1@users.noreply.github.com>
2024-07-15 14:52:35 +0500

    final commits

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import Navbar from "@/components/Header";
 import WhatsAppButton from "@/components/whatsappbutton";
 import OfferDialog from "@/components/offersdialogue";
 import Head from "next/head";
-import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,18 +18,29 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=GT-WPL9D6WM"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'GT-WPL9D6WM');
+            `,
+          }}
+        />
       </Head>
       <body>
         <div className="fixed-navbar">
           <Navbar />
           <WhatsAppButton />
           <OfferDialog 
-            googleMapsUrl="https://maps.app.goo.gl/owFNrAMn1L1z8WJHA"
+            googleMapsUrl="https://maps.app.goo.gl/owFNrAMn1L1z8WJHA" // Replace with your actual Google Maps URL
             locationName="Swabi Laundry and Dry Cleaning Service"
           />
         </div>
         <div className={inter.className}>{children}</div>
-        <GoogleAnalytics gaId="GTM-M2474PBS" />
       </body>
     </html>
   );
